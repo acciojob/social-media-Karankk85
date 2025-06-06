b2fa85e886346fe5d34ef23b77bd570da6da69e8
Karankk85 <karkk8533@gmail.com>
2025-06-06 11:02:53 +0530

    Update index.js
    as

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App"; // Updated path
+import { BrowserRouter } from "react-router-dom";
+import { mount } from "cypress/react";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+mount(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
